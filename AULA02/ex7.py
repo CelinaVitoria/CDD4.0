560ce0686c490ce39d283ab90859a034f6124b47
CelinaVitoria <164938493+CelinaVitoria@users.noreply.github.com>
2024-04-08 14:07:39 -0300

    Update ex7.py

--- AULA02/ex7.py
+++ AULA02/ex7.py
@@ -3,6 +3,12 @@ minuto1 = int(input("Primeira minuto: "))
 hora2 = int(input("Segunda Hora: "))
 minuto2 = int(input("Segunda minuto: "))
 
+if hora1 >= 12:
+   hora1 -= 12
+
+if hora2 >= 12:
+    horas -= 12
+
 horas = hora1 + hora2
 minutos = minuto1 + minuto2
 
@@ -10,14 +16,7 @@ if minutos >= 60:
     minutos -= 60
     horas += 1
 
-if horas > 12:
-   horas -= 12
-elif horas < 12:
-    horas += 12
-
 if minutos < 10:
     print(f"{int(horas)}:0{minutos}")
 else:
     print(f"{int(horas)}:{minutos}")
-
-
