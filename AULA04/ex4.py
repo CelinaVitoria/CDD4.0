4b764fb66ba2954d0a8a44601c4685f5ad25e42c
Celina Vitoria <164938493+CelinaVitoria@users.noreply.github.com>
2024-04-16 16:20:32 -0300

    Update ex4.py

--- AULA04/ex4.py
+++ AULA04/ex4.py
@@ -4,6 +4,7 @@ for x in range(1,11):
     num = int(input(f"Digite o número {x}:"))
     if num >= 10 and num <= 20:
         dentro = dentro + 1
-    fora = fora + 1
+    else:
+        fora = fora + 1
 print(f"{dentro} números estão dentro do intervalo")
 print(f" {fora} estão fora do intervalo")
