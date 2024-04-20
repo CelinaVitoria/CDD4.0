43e3bae14fe8f0f4d109ed943ed77ecd9bf49203
Celina Vitoria <164938493+CelinaVitoria@users.noreply.github.com>
2024-04-19 21:47:41 -0300

    Add files via upload

---
+++ AULA04/ex9.py
@@ -0,0 +1,10 @@
+ss = 1234
+x = 0
+senha = int(input("digite a senha:"))
+while senha != ss:
+    senha = int(input("digite a senha:"))
+    x = x + 1
+    if x == 2 and ss != senha:
+        print("senha bloqueada")
+        break
+print("correto!")
