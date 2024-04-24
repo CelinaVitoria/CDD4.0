58201c326d8596a60e9046e4964a5cd2f16c35a8
Celina Vitoria <164938493+CelinaVitoria@users.noreply.github.com>
2024-04-24 15:59:36 -0300

    Update ex6.py

--- Revisão/ex6.py
+++ Revisão/ex6.py
@@ -1,6 +1,8 @@
 pedido = 0
 while pedido != 3:
     pedido = int(input("Digite 1 para a área do triângulo ou 2 para a area do quadrado e três para sair:"))
+    if pedido > 3 or pedido <1:
+        print("Número inválido")
     if pedido == 1:
         lado = float(input("Digite o valor do lado do quadrado:"))
         area = lado**2
