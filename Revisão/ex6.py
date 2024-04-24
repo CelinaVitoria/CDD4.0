8345b0a8b7928df927694fdf39550b8b62334337
Celina Vitoria <164938493+CelinaVitoria@users.noreply.github.com>
2024-04-24 15:52:55 -0300

    Update ex6.py

--- Revisão/ex6.py
+++ Revisão/ex6.py
@@ -1,4 +1,12 @@
-base = float(input("Digite o valor da base do triângulo:"))
-altura = float(input("Digite o valor da altura do triângulo:"))
-área = (base * altura)/2
-print(área)
+pedido = 0
+while pedido != 3:
+    pedido = int(input("Digite 1 para a área do triângulo ou 2 para a area do quadrado e três para sair:"))
+    if pedido == 1:
+        lado = float(input("Digite o valor do lado do quadrado:"))
+        area = lado**2
+        print(area)
+    elif pedido == 2:
+        base = float(input("Digite o valor da base do triângulo:"))
+        altura = float(input("Digite o valor da altura do triângulo:"))
+        area = (base * altura)/2
+        print(area)
