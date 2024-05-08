de60b608a64c35cef5b92cbfb0fab596744e25bc
Celina Vitoria <164938493+CelinaVitoria@users.noreply.github.com>
2024-05-08 17:02:26 -0300

    Add files via upload

---
+++ aula06/ex1.py
@@ -0,0 +1,33 @@
+def cadastro():
+    nomes = ["","","","",""]
+    senhas = ["","","","",""]
+    for x in range(5):
+        nomes = input("digite nome:")
+        senhas = int(input("digite a senha:"))
+def imprime_nome(nome):
+    print(f"nome: {nome}")
+def piramide(n):
+    for x in range(1,n+1):
+        for y in range(1, x+1):
+            print(x,end = " ")
+        print()
+def piramide2(j):
+    for x in range(1,j+1):
+        for y in range(1,x+1):
+            print(y,end = " ")
+        print()
+def vogais(texto):
+    cont1 = 0
+    cont2 = 0
+    for z in texto:
+        if z == " ":
+            cont1 = cont1 + 1
+    for x in texto:
+        if x in "aeiouAEIOU":
+            cont2 = cont2 + 1
+    print(cont1)
+    print(cont2)
+texto = "o rato roeu a roupa do rei de roma"
+def estoque(nome, quant, valor):
+    total = quant * valor
+    return total
---
+++ aula06/ex2.py
@@ -0,0 +1,10 @@
+'''from ex1 import vogais
+h = "eu to doido"
+vogais(h)
+'''
+"''IMPORTAR TUDO: from ex1 import *''"
+'''from ex1 import piramide
+piramide(5)'''
+
+from ex1 import estoque
+estoque("arroz",20,5)
