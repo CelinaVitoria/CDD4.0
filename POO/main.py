91f09ca02deddd24263ead9a6b943f6e2204c7b2
Celina Vitoria <164938493+CelinaVitoria@users.noreply.github.com>
2024-05-18 21:49:26 -0300

    Update main.py

--- POO/main.py
+++ POO/main.py
@@ -1,26 +1,8 @@
-class Pessoa():
-    def __init__(self, nomeAluno, pesoAluno, idadeAluno,comendo=False,falando=False,dormindo=False):
-        self.nome = nomeAluno
-        self.peso = pesoAluno
-        self.idade = idadeAluno
-        self.comendo = comendo
-        self.falando = falando
-        self.dormindo = dormindo
-    def comer(self,alimento):
-        print(f"{self.nome} foi comer {alimento}")
-        self.comendo=True
-    def parardecomer(self):
-        print(f"{self.nome} parou de comer")
-        self.comendo = False
-    def falar(self,fala):
-        print(f"{self.nome} falou {fala}")
-        self.falando = True
-    def parardefalar(self):
-        print(f"{self.nome} parou de falar")
-        self.comendo = False
-    def dormir(self,dorme):
-        print(f"{self.nome} {dorme}")
-        self.dormindo = True
-    def parardedormir(self):
-        print(f"{self.nome} parou de dormir")
-        self.comendo = False
+from classes import *
+p1 = Pessoa("vi",70,18)
+p1.dormir()
+p1.parardedormir()
+p1.falar()
+p1.parardefalar()
+p1.comer("doce")
+p1.parardecomer()
