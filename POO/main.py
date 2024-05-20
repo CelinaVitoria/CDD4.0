a0397026b9ca7db3bfb32d0bb6698cc3526cbc6b
Celina Vitoria <164938493+CelinaVitoria@users.noreply.github.com>
2024-05-20 15:34:30 -0300

    Update main.py

--- POO/main.py
+++ POO/main.py
@@ -1,4 +1,4 @@
-from classes import *
+from classes import Pessoa
 p1 = Pessoa("vi",70,18)
 p1.dormir()
 p1.parardedormir()
@@ -6,3 +6,10 @@ p1.falar()
 p1.parardefalar()
 p1.comer("doce")
 p1.parardecomer()
+
+from classes import conta
+p1 = conta(1,"vi","poupança")
+p1.ativar("ativar")
+p1.depositar(1800)
+p1.sacar(1000)
+p1.verificar()
