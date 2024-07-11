48d386d55d457704cf427a9d7ac2a8ef0ed413ec
Celina Vitoria <164938493+CelinaVitoria@users.noreply.github.com>
2024-07-11 16:38:05 -0300

    Add files via upload

---
+++ Metodos/calculadora.java
@@ -0,0 +1,11 @@
+package Metodos;
+
+public class calculadora {
+
+	public static void main(String[] args) {
+	calcularMetodos c1 = new calcularMetodos();
+	System.out.println(c1.somar(5,6,7));
+	System.out.println(c1.somar(5,6));
+	}
+
+}
